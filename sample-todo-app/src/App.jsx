a4c48470a2fe60328c222789e0dcb9d0409ac00f
csepulv <csepulv@gmail.com>
2025-06-25 18:51:21 -0700

    starting vite sample app

---
+++ sample-todo-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from Vite + React!</h1>
+      <p>This is the sample todo app.</p>
+    </div>
+  );
+}
+
+export default App; 
